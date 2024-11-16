c68620e6e96b3bb61ebf1038548535b9ea38145a
kamal7k <kunwarkamal7k@gmail.com>
2024-11-16 14:50:16 +0545

    ch

--- src/constants/api_urls.js
+++ src/constants/api_urls.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'http://192.168.254.2:8000/api';
+export const baseUrl = 'https://vyayamshala.onrender.com/api';
 
-export const imageUrl = 'http://192.168.254.2:8000';
+export const imageUrl = 'https://vyayamshala.onrender.com';
